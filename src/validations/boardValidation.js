6b10f260b072c06aab6000e318b4d6d73c73e47e
Nguyen Ngoc Tai <23020701@vnu.edu.vn>
2025-04-03 22:06:23 +0700

    validattion

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,33 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required (trungquandev)',
+      'string.empty': 'Title is not allowed to be empty (trungquandev)',
+      'string.min': 'Title min 3 chars (trungquandev)',
+      'string.max': 'Title max 50 chars (trungquandev)',
+      'string.trim': 'Title must not have leading or trailing whitespace (trungquandev)'
+    }),
+    description:Joi.string().required().min(3).max(256).trim().strict()
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, { abortEarly:false })
+    //validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang controller
+    // next()
+    // res.status(StatusCodes.CREATED).json({ message:'Note: API post list post' })
+
+  } catch (err) {
+    console.log(err)
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(err).message
+    })
+}
+
+}
+
+export const boardValidation = {
+  createNew
+}
