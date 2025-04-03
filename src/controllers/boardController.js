7a4bddabbebb2a917b2182e0ab0d25e4cd413504
Nguyen Ngoc Tai <23020701@vnu.edu.vn>
2025-04-03 22:17:49 +0700

    update_controller_(next)

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,25 +1,23 @@
 import { StatusCodes } from 'http-status-codes'
 
-const createNew = async (req, res,next) => {
+const createNew = async (req, res, next) => {
   try {
-    console.log("req.body:" + req.body)
-    console.log('req.body: ', req.body);
-    console.log('req.query: ', req.query);
-    console.log('req.params: ', req.params);
-    // console.log('req.files: ', req.files);
-    // console.log('req.cookies: ', req.cookies);
-    // console.log('req.jwtDecoded: ', req.jwtDecoded);
+    console.log('req.body:' + req.body)
+    console.log('req.body: ', req.body)
+    console.log('req.query: ', req.query)
+    console.log('req.params: ', req.params)
+    // console.log('req.files: ', req.files)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
     //Tiếp đến là điều hướng dữ liệu sang tầng service
 
     //Có kết quả thì trả về Client
     res.status(StatusCodes.CREATED).json({ message:'Post from controller: API post list post' })
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(err)
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error:err.message
     })
-}
-
-export const boardController = {
-  createNew
-}
+}}
+export const boardController = { createNew }
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 
-const createNew = async (req, res) => {
+const createNew = async (req, res,next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict().messages({
       'any.required': 'Title is required (trungquandev)',
@@ -16,9 +16,9 @@ const createNew = async (req, res) => {
   try {
     await correctCondition.validateAsync(req.body, { abortEarly:false })
     //validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang controller
-    // next()
+    // eslint-disable-next-line no-undef
+    next()
     // res.status(StatusCodes.CREATED).json({ message:'Note: API post list post' })
-
   } catch (err) {
     console.log(err)
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
